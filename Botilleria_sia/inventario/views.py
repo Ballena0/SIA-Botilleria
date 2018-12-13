151524cb60ba4b412e486853dfc3026881d7b1f5
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2018-12-13 00:26:30 -0300

    paso previo a insertar templates

--- Botilleria_sia/inventario/views.py
+++ Botilleria_sia/inventario/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Template, Context
+from django.views.generic import CreateView, TemplateView
 from django.http import JsonResponse
 # Create your views here.
 
-def index(request):
-    ##numbers = [int (i) for i in request.GET['numbers'].split(',')]
-    ##sorteados= sorted(numbers)
-    ##return JsonResponse(sorteados, safe=False)
-    return HttpResponse("esta es la primera vista, donde aparecen productos y todo eso blaa bla")
+def index(TemplateView):
+    template_name = 'index.html'
+    return HttpResponse(template_name)
 
 def productos(request):
-    return HttpResponse("esta es otra vista, la de los productos" )
+    return HttpResponse("esta es otra vista, la de los productos")
 
 def tipo_formato(request):
     return HttpResponse("Esta es la de los formatos")
