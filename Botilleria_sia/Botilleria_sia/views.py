942ff12b3f49e2418fc3d370d0da8a9ed4f028a3
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2018-12-20 13:58:39 -0300

    adding some views, need to fix /inventario view

---
+++ Botilleria_sia/Botilleria_sia/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def indexindex(request):
+    return HttpResponse("login")
--- Botilleria_sia/inventario/views.py
+++ Botilleria_sia/inventario/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 # Create your views here.
+from inventario.models import PRODUCTO
+
 
 def index(request):
-    template = loader.get_template('base.html')
     context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'base.html', context)
 
-def productos(request):
-    return HttpResponse("esta es otra vista, la de los productos")
+def productos(request, ID_PROD):
+    producto = PRODUCTO.objects.get(pk=ID_PROD)
+    context = { producto }
+    return render(request, 'productos.html', context)
 
 def tipo_formato(request):
-    return HttpResponse("Esta es la de los formatos")
+    context = {}
+    return render(request,'tipoformato.html', context)
