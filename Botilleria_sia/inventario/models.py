e58a0bf8d6ff6f80901cf9879926743971bdcca5
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2018-12-23 20:10:56 -0300

    cambiando modelos

--- Botilleria_sia/inventario/models.py
+++ Botilleria_sia/inventario/models.py
@@ -4,21 +4,18 @@ from django.db import models
 
 class FORMATO(models.Model):
     FORMATO_ID = models.IntegerField(primary_key=True)
-    CANTIDAD = models.IntegerField(default=0)
-    CANT_P = models.IntegerField(default=0)
-    TIPO_FORMATO = models.CharField(max_length=50)
+    UNIDADES = models.CharField(max_length=50)
     DESCRIPCION_FOR = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.TIPO_FORMATO
+        return self.UNIDADES
 
 class PRODUCTO(models.Model):
-    FORM_PROD = models.ForeignKey(FORMATO, on_delete=models.CASCADE)
+    FORMAT_PROD = models.ForeignKey(FORMATO, on_delete=models.CASCADE)
     ID_PROD = models.IntegerField(primary_key=True)
     NOMBRE_PROD = models.CharField(max_length=50)
     PRECIO = models.IntegerField(default=0)
     STOCK = models.IntegerField(default=0)
-    DESCRIPCION_PROD = models.CharField(max_length=50)
 
     def __str__(self):
         return self.NOMBRE_PROD
