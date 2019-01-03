8857e0401905d1874091f9d4fcbfbbc5202e1f7a
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2019-01-03 18:02:45 -0300

    Tabla de productos

--- Botilleria_sia/inventario/views.py
+++ Botilleria_sia/inventario/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import PRODUCTO, PROVEEDOR, RegistroBodega
 
 def index(request):
     context = {}
     return render(request, 'inventario/base.html', context)
 
 def productos(request):
-    return render(request, 'inventario/productos.html')
+    show_id= PRODUCTO.objects.all()
+    return render(request, 'inventario/productos.html', {'show_id': show_id})
 
 def tipo_formato(request):
     context = {}
