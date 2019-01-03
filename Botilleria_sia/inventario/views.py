9ec7da0dad723abf89c4ac86e9e0832d359c3ebe
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2019-01-03 15:17:42 -0300

    adding branches to master

--- Botilleria_sia/inventario/views.py
+++ Botilleria_sia/inventario/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from .models import PRODUCTO, FORMATO
 
 def index(request):
     context = {}
     return render(request, 'inventario/base.html', context)
 
 def productos(request):
-    ## producto = PRODUCTO.objects.get(pk=ID_PROD)
-    ## context = {producto}
-    return render(request, 'productos.html')
+    return render(request, 'inventario/productos.html')
 
 def tipo_formato(request):
     context = {}
-    return render(request,'tipoformato.html', context)
+    return render(request,'inventario/tipoformato.html', context)
+
